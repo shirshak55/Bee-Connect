8b54b2cedd8555d898d6de88702c6663e36b279d
Shirshak Bajgain <bloggervista@gmail.com>
2017-01-25 09:06:32 +0545

    User admin fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   has_many :messages, dependent: :destroy
 
   has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
-  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'receiver_id'
   has_many :personal_messages, dependent: :destroy
 
   has_many :notifications,foreign_key: :recipient_id
