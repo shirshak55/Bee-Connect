3b0ef5de1cded29d2981d3870ef4927e8e88db0f
Shirshak Bajgain <bloggervista@gmail.com>
2017-01-30 12:22:00 +0545

    CHacheddddd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,12 +16,6 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-  config.static_cache_control = 'public, max-age=1000'
-
-  config.public_file_server.headers = {
-    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
-    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
-  }
 
   # config.force_ssl = true
 
